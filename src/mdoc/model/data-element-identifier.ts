643c7bcff34c54fd7f1f5a64529af15dee36b5e5
Berend Sliedrecht <berend@animo.id>
2025-04-25 14:18:57 +0200

    chore: added test for device-request
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- src/mdoc/model/data-element-identifier.ts
+++ src/mdoc/model/data-element-identifier.ts
@@ -1,7 +1,1 @@
-import { CborStructure } from '../../cbor'
-
-export class DataElementIdentifier extends CborStructure {
-  public encodedStructure(): unknown {
-    throw new Error('Method not implemented.')
-  }
-}
+export type DataElementIdentifier = string
