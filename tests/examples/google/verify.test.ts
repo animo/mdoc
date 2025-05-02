85b87af923a99a7e74eae35d63ccf9c0ebdd089a
Berend Sliedrecht <berend@animo.id>
2025-05-02 12:05:34 +0200

    chore: make google test work again
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

---
+++ tests/examples/google/verify.test.ts
@@ -0,0 +1,35 @@
+import { describe, it } from 'vitest'
+import { DeviceResponseOld, Verifier } from '../../../src'
+import { mdocContext } from '../../context'
+import { deviceResponse } from './deviceResponse'
+import { rootCertificate } from './rootCertificate'
+import { signingCertificate } from './signingCertificate'
+
+describe('Google CM Wallet mdoc implementation', () => {
+  it('should verify DC API DeviceResponse from Google CM Wallet', async () => {
+    const verifierGeneratedNonce = 'UwQek7MemM55VM2Lc7UPPsdsxa-vejebSUo75B_G7vk'
+    const origin = 'https://ellis-occurrence-ac-smoking.trycloudflare.com'
+    const clientId = `web-origin:${origin}`
+
+    const verifier = new Verifier()
+    await verifier.verifyDeviceResponse(
+      {
+        trustedCertificates: [
+          new Uint8Array(rootCertificate.rawData),
+          // FIXME: verification fails when only trusting root certificate. We need
+          // to trust the signing certificate for now
+          new Uint8Array(signingCertificate.rawData),
+        ],
+        encodedDeviceResponse: deviceResponse,
+        encodedSessionTranscript: await DeviceResponseOld.calculateSessionTranscriptBytesForOID4VPDCApi({
+          context: mdocContext,
+          origin,
+          clientId,
+          verifierGeneratedNonce,
+        }),
+        now: new Date('2025-02-20'),
+      },
+      mdocContext
+    )
+  })
+})
